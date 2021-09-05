1b4f77b64811d751820ff135a2f9a4024a46fa40
Ghaiyur <ghaiyur@gmail.com>
2021-09-05 11:37:54 +0530

    Player Movement

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - [x] Sprites
     - [x] Added background
     - [x] Added player 
+- [x] Player movement 
+    - [x] Checks if the key is pressed love.keyboard.isDown
+        - [x] and then any operations can be performed under that func
+        - [x] increase or decrease x and y of the player 
+    - [x] Added player.speed
--- main.lua
+++ main.lua
@@ -9,11 +9,27 @@ function love.load()
     -- Player pos
     player = {}
     player.x = love.graphics.getWidth() / 2
-    player.y = love.graphics.getHeight() /2
-    
+    player.y = love.graphics.getHeight() / 2
+    player.speed = 2.5
 end
 
 function love.update()
+    -- Right movement
+    if love.keyboard.isDown('d') then
+        player.x = player.x + player.speed
+    end
+    -- left movement
+    if love.keyboard.isDown('a') then
+        player.x = player.x - player.speed
+    end
+    -- up movement
+    if love.keyboard.isDown('w') then
+        player.y = player.y - player.speed
+    end
+    -- down movement
+    if love.keyboard.isDown('s') then
+        player.y = player.y + player.speed
+    end
     
 end
 
